63ec311e4b502910f32718e23e00a12ab30e917a
frazking <ali.ahmed@example.com>
2026-03-16 07:23:25 +0500

    Branding finalized as FARAZ KING

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'Gifted~lyYSxGjHJXYU',
     OWNER_NUMBER: '263775664225',
-    OWNER_NAME: 'Ali Ahmed',
+    OWNER_NAME: 'FARAZ KING',
     BOT_NAME: 'FRAZ-KING-MD',
-    YT_CHANNEL: 'https://youtube.com/@CineOfficial', // Aapka Cine Official Channel
+    YT_CHANNEL: 'https://youtube.com/@CineOfficial',
     PREFIX: '.',
     MODE: 'public',
 };
